07d30676b5dad9d1077e2e8fe893699c11a31c5b
Jincheng Du <edwardujc818@gmail.com>
2019-04-05 15:38:14 -0700

    similar tab implemented

--- src/app/details-table/similar-tab/similar-tab.component.ts
+++ src/app/details-table/similar-tab/similar-tab.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, OnChanges, OnInit} from '@angular/core';
+import {catchError} from 'rxjs/operators';
 
 @Component({
   selector: 'app-similar-tab',
@@ -7,18 +8,18 @@ import {Component, Input, OnChanges, OnInit} from '@angular/core';
 })
 export class SimilarTabComponent implements OnChanges {
   @Input() similar: {};
+  items = [];
 
-  // keyTypes = ['Default', 'Product Name', 'Days Left', 'Price', 'Shipping Cost'];
-  // selectedKeyType = 'Default';
+  keyTypes = ['Default', 'Product Name', 'Days Left', 'Price', 'Shipping Cost'];
+  selectedKeyType = 'Default';
 
-  keyTypes = ['id', 'name', 'genre'];
-  selectedKeyType = 'id';
-
+  OrderKey = 'id';
+  orderKeyEnabled = false;
 
   orderTypes = ['Ascending', 'Descending'];
   selectedOrderType = 'Ascending';
 
-  reverse: false;
+  reverse = false;
 
   // -----------------------------------------------------------
   games = [
@@ -27,48 +28,6 @@ export class SimilarTabComponent implements OnChanges {
       "name": "DOTA 2",
       "genre": "Strategy"
     },
-    {
-      "id":"2",
-      "name": "AOE 3",
-      "genre": "Strategy"
-    },
-    {
-      "id":"3",
-      "name": "GTA 5",
-      "genre": "RPG"
-    },
-    {
-      "id":"4",
-      "name": "Far Cry 3",
-      "genre": "Action"
-    },
-    {
-      "id":"5",
-      "name": "GTA San Andreas",
-      "genre": "RPG"
-    },
-    {
-      "id":"6",
-      "name": "Hitman",
-      "genre": "Action"
-    },
-    {
-      "id":"7",
-      "name": "NFS MW",
-      "genre": "Sport"
-    },{
-      "id":"8",
-      "name": "Fifa 16",
-      "genre": "Sport"
-    },{
-      "id":"9",
-      "name": "NFS Sen 2",
-      "genre": "Sport"
-    },{
-      "id":"10",
-      "name": "Witcher Assasins on King",
-      "genre": "Adventure"
-    }
   ]
   // -----------------------------------------------------------
 
@@ -84,40 +43,99 @@ export class SimilarTabComponent implements OnChanges {
     console.log('setOrderType to ', deviceValue, this.reverse);
   }
 
+  setKeyType(deviceValue) {
+    switch (deviceValue) {
+      case 'Default': {
+        this.OrderKey = 'index';
+        this.orderKeyEnabled = false;
+        break;
+      }
+      case 'Product Name': {
+        this.OrderKey = 'title';
+        this.orderKeyEnabled = true;
+        break;
+      }
+      case 'Days Left': {
+        this.OrderKey = 'daysLeft';
+        this.orderKeyEnabled = true;
+        break;
+      }
+      case 'Price': {
+        this.OrderKey = 'price';
+        this.orderKeyEnabled = true;
+        break;
+      }
+      case 'Shipping Cost': {
+        this.OrderKey = 'shippingCost';
+        this.orderKeyEnabled = true;
+        break;
+      }
+    }
+    console.log('setKeyType to ', deviceValue, this.OrderKey);
+
+  }
+
+  openTab(url) {
+    const win = window.open(url, '_blank');
+    win.focus();
+  }
+
   ngOnChanges() {
-    console.log('similar: ', this.similar);
+    console.log('received similar: ', this.similar);
+    try {
+      this.items = [];
+      let i = 0;
+      for ( let item of this.similar['getSimilarItemsResponse']['itemRecommendations']['item']) {
+        let tmpJson = {};
+        tmpJson['index'] = i;
+        i++;
+        try {
+          tmpJson['title'] = item['title'];
+        } catch (e) {
+          tmpJson['title'] = '';
+        }
+        try {
+          tmpJson['viewItemURL'] = item['viewItemURL'];
+        } catch (e) {
+          tmpJson['viewItemURL'] = '';
+        }
+        try {
+          tmpJson['price'] = +item['buyItNowPrice']['__value__'];
+        } catch (e) {
+          tmpJson['price'] = '';
+        }
+        try {
+          tmpJson['shippingCost'] = +item['shippingCost']['__value__'];
+        } catch (e) {
+          tmpJson['shippingCost'] = '';
+        }
+        try {
+          const timeLeftStr = item['timeLeft'];
+          const pIndex = timeLeftStr.indexOf('P');
+          const dIndex = timeLeftStr.indexOf('D');
+          // console.log('timeLeftStr:', timeLeftStr);
+          // console.log('pIndex:', pIndex);
+          // console.log('dIndex:', dIndex);
+
+          tmpJson['daysLeft'] = +timeLeftStr.slice(pIndex + 1, dIndex);
+
+          // console.log('tmpJson.daysLeft:', tmpJson['daysLeft']);
+        } catch (e) {
+          // console.log(e);
+          tmpJson['daysLeft'] = '';
+        }
+        try {
+          tmpJson['image'] = item['imageURL'];
+        } catch (e) {
+          tmpJson['image'] = '';
+        }
+        this.items.push(tmpJson);
+      }
+      console.log('this.items: ', this.items);
+    } catch (e) {
+      this.items = [];
+      return;
+    }
   }
 
 }
-
-// setKeyDefault() {}
-// setKeyProductName() {}
-// setKeyDays() {}
-// setKeyPrice() {}
-// setKeyShippingCost() {}
-//
-// setKeyType(i) {
-//   this.selectedKeyType = i;
-//   console.log('seletedKeyType: ', this.selectedKeyType);
-//   switch (i) {
-//     case 0: {
-//       this.setKeyDefault();
-//       return;
-//     }
-//     case 1: {
-//       this.setKeyProductName();
-//       return;
-//     }
-//     case 2: {
-//       this.setKeyDays();
-//       return;
-//     }
-//     case 3: {
-//       this.setKeyPrice();
-//       return;
-//     }
-//     case 4: {
-//       this.setKeyShippingCost();
-//     }
-//   }
-// }
